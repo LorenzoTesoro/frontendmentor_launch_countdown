bf77a9c611bd3602b9d025a19cca32c726bbd4b5
LorenzoTesoro <lorenzotesoro2@gmail.com>
2023-01-18 16:11:14 +0100

    add event listeners

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,50 +6,66 @@ const hoursElement = document.querySelector('.hours .number');
 const minutesElement = document.querySelector('.minutes .number');
 const secondsElement = document.querySelector('.seconds .number');
 // set variables for days, hours, minutes, seconds
-let days = 11;
-let hours = Number('0' + 0);
-let minutes = Number('0' + 0);
-let seconds = Number('0' + 0);
-// timer counter
-setInterval(function(){
-    // insert markup into squares
-    secondsElement.innerHTML = seconds;
-    minutesElement.innerHTML = minutes;
-    hoursElement.innerHTML = hours;
-    daysElement.innerHTML = days;
-    // initial state
-    if((hours - '0') === 0 && (minutes - '0') === 0 && (seconds - '0') === 0){
-        days--;
-        hours = 23;
-        minutes = 59;
-        seconds = 59;
-    }
+let days = 14;
+let hours ='0' + 0;
+let minutes = '0' + 0;
+let seconds = '0' + 0;
+// insert static markup into squares
+secondsElement.innerHTML = seconds;
+minutesElement.innerHTML = minutes;
+hoursElement.innerHTML = hours;
+daysElement.innerHTML = days;
 
-    seconds--
+// select buttons
+const btnStart = document.querySelector('.start');
+const btnStop = document.querySelector('.stop');
 
-    if(seconds < 10){
-        seconds = '0' + seconds;
-    }
-
-    if(seconds == 0){        
-        seconds = 59; 
-        minutes-- 
-        if(minutes < 10){
-            minutes = '0' + minutes;
-        }
-        if(minutes == 0){
+// add event listener to controls
+btnStart.addEventListener('click', function(){
+    const intervalId = setInterval(function(){
+        // initial state
+        secondsElement.innerHTML = seconds;
+        minutesElement.innerHTML = minutes;
+        hoursElement.innerHTML = hours;
+        daysElement.innerHTML = days; 
+    
+        if((hours - '0') == 0 && (minutes - '0') == 0 && (seconds - '0') == 0){
+            days--;
+            hours = 23;
             minutes = 59;
-            hours--;
-            if(hours < 10){
-                hours = '0' + hours;
+            seconds = 59;
+        }
+    
+        seconds--
+    
+        if(seconds < 10){
+            seconds = '0' + seconds;
+        }
+    
+        if(seconds == 0){        
+            seconds = 59; 
+            minutes-- 
+            if(minutes < 10){
+                minutes = '0' + minutes;
             }
-            if(hours == 0){
-                hours = 24;
-                days--;
-                if(days < 10){
-                    days = '0' + days;
+            if(minutes == 0){
+                minutes = 59;
+                hours--;
+                if(hours < 10){
+                    hours = '0' + hours;
                 }
-            }
-        }   
-    }
-}, 0.001)
+                if(hours == 0){
+                    hours = 24;
+                    days--;
+                    if(days < 10){
+                        days = '0' + days;
+                    }
+                }
+            }   
+        }
+    }, 1000)
+
+    btnStop.addEventListener('click', function(){
+        clearInterval(intervalId);
+    })
+})
